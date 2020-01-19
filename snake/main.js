4d34c32a28d1539319257c1ed3481451ade75e3c
Sam Davydov <hildburghauser99@mail.ru>
2020-01-19 22:26:22 +0300

    Создал функцию, которая генерирует случайные координаты по ося от 1 до 10. Создал массив, в котором хранятся три элемента поля, которые занимает змея в начале игры. Для каждого элемента поля добавил класс scaleBody и класс для головы змеи head. Все классы опсаны в style.css

--- snake/main.js
+++ snake/main.js
@@ -20,3 +20,32 @@ for (i = 0; i < excel.length; i++) {
   excel[i].setAttribute('posY', y);
   x++;
 }
+
+// let x = 1, y = 1;
+// for (i = 0; i < excel.length; i++) {
+//   if (x > 10) {
+//     x = 1;
+//     y++;
+//   }
+//   excel[i].setAttribute('posX', x);
+//   excel[i].setAttribute('posY', y);
+//   x++;
+// }
+
+function generateSnake() {
+  let posX = Math.round(Math.random() * (10 - 3) + 3);
+  let posY   = Math.round(Math.random() * (10 -1) + 1);
+  return [posX, posY];
+}
+
+let coordinates = generateSnake();
+let snakeBody = [document.querySelector('[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'),
+  document.querySelector('[posX = "' + (coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'),
+  document.querySelector('[posX = "' + (coordinates[0] - 2) + '"][posY = "' + coordinates[1] + '"]')];
+
+for (i = 0; i < snakeBody.length; i++) {
+  snakeBody[i].classList.add('scaleBody')
+}
+snakeBody[0].classList.add('head');
+
+// console.log(snakeBody)
